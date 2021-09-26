74f64e0e7353070ace246077688927f3b8c039a1
Guilherme Marques <guilhermemarques97@poli.ufrj.br>
2021-09-26 19:21:13 -0300

    secret

--- agenda/settings.py
+++ agenda/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$49(xl6e@-q*2mc&^42wbkkav(qz(rc+hexhp!l*b3t-1fhw^s'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
